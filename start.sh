4039fe4dc27c60c71d151b188a6a8ed93861ee41
Michael Perttula <mperttula@mikeP-mac.local>
2018-04-16 15:16:22 -0400

    pass in httpuser directly in curl

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 echo "Loging into $NAGIOSHOSTNAME with user $NAGIOSUNAME, checking host $NAGIOSHOSTCHECK"
 
-echo "user=${NAGIOSUNAME}:${NAGIOSPASS}" | curl -k "https://$NAGIOSHOSTNAME/nagios/cgi-bin/cmd.cgi?cmd_typ=30&cmd_mod=2&host=$NAGIOSHOSTCHECK&service=swarm_launch_container&plugin_state=0&plugin_output='Swarm is running containers'&btnSubmit=Commit"
+curl -k --user $NAGIOSUNAME:$NAGIOSPASS "https://$NAGIOSHOSTNAME/nagios/cgi-bin/cmd.cgi?cmd_typ=30&cmd_mod=2&host=$NAGIOSHOSTCHECK&service=swarm_launch_container&
+plugin_state=0&plugin_output=Swarm_run_operational&btnSubmit=Commit"
