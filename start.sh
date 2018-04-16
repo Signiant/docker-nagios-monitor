8a03bbe1afa061dd06902692892ae44806b5d5f9
Michael Perttula <mperttula@mikeP-mac.local>
2018-04-16 15:21:20 -0400

    Removed bad newline

--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@
 
 echo "Loging into $NAGIOSHOSTNAME with user $NAGIOSUNAME, checking host $NAGIOSHOSTCHECK"
 
-curl -k --user $NAGIOSUNAME:$NAGIOSPASS "https://$NAGIOSHOSTNAME/nagios/cgi-bin/cmd.cgi?cmd_typ=30&cmd_mod=2&host=$NAGIOSHOSTCHECK&service=swarm_launch_container&
-plugin_state=0&plugin_output=Swarm_run_operational&btnSubmit=Commit"
+curl -k --user $NAGIOSUNAME:$NAGIOSPASS "https://$NAGIOSHOSTNAME/nagios/cgi-bin/cmd.cgi?cmd_typ=30&cmd_mod=2&host=$NAGIOSHOSTCHECK&service=swarm_launch_container&plugin_state=0&plugin_output=Swarm_run_operational&btnSubmit=Commit"
