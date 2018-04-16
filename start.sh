6be856e778c05175b35baa9fca0ff8b91b504ca6
Michael Perttula <mperttula@mikeP-mac.local>
2018-04-16 14:39:55 -0400

    Change case of k arguement

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Loging into $NAGIOSHOSTNAME with user $NAGIOSUNAME, checking host $NAGIOSHOSTCHECK"
-
-echo 'user="${NAGIOSUNAME}:${NAGIOSPASS}"' | curl -K - "https://$NAGIOSHOSTNAME/nagios/cgi-bin/cmd.cgi?cmd_typ=30&cmd_mod=2&host=$NAGIOSHOSTCHECK&service=swarm_launch_container&plugin_state=0&plugin_output='Swarm is running containers'&btnSubmit=Commit"
+echo "https://$NAGIOSHOSTNAME/nagios/cgi-bin/cmd.cgi?cmd_typ=30&cmd_mod=2&host=$NAGIOSHOSTCHECK&service=swarm_launch_container&plugin_state=0&plugin_output='Swarm is running containers'&btnSubmit=Commit"
+echo 'user="${NAGIOSUNAME}:${NAGIOSPASS}"' | curl -k - "https://$NAGIOSHOSTNAME/nagios/cgi-bin/cmd.cgi?cmd_typ=30&cmd_mod=2&host=$NAGIOSHOSTCHECK&service=swarm_launch_container&plugin_state=0&plugin_output='Swarm is running containers'&btnSubmit=Commit"
